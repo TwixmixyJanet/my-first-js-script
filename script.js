f71874fc403b6730ae7cbaebb532be1b8038d7d2
Janet Webster <twixmixy@gmail.com>
2023-02-13 16:17:55 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Write your JavaScript below this line
+var name = "Janet";
+var snack = "Snickers";
+console.log(
+  "👽 Greetings travelers, my name is " +
+    name +
+    " and I wish I had a " +
+    snack +
+    " right now."
+);
